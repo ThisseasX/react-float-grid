bb22a9292319090407f50c392c0d6e2d5fb89f6b
Thisseas Xanthopoulos <thisseasx@yahoo.com>
2020-09-21 17:32:53 +0300

    Updates

---
+++ precache-manifest.85e97ad8b0e250b857e84bb00f1b3618.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13113ca7ac47251ccd6789d6ba920080",
+    "url": "/react-float-grid/index.html"
+  },
+  {
+    "revision": "7261e2271804fc09fd51",
+    "url": "/react-float-grid/static/js/2.04c16dbe.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/react-float-grid/static/js/2.04c16dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e450869059a701bc80c",
+    "url": "/react-float-grid/static/js/main.e32a2d36.chunk.js"
+  },
+  {
+    "revision": "2521d656b68fce487c73",
+    "url": "/react-float-grid/static/js/runtime-main.78f83244.js"
+  }
+]);
